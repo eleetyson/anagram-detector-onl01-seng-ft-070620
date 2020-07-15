ae4609121e7c4ad392a69ef6272144459e336926
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-15 23:29:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,9 +12,6 @@ class Anagram
     words.select do |i|  
       i.split.sort == @word.split.sort
     end
-    # loop through every word in the words array
-    # turn the word into an array, sort it
-    # check whether it's equal to @word, turned into an array and sorted
   end
   
 end
